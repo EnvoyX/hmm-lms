91e3c745d145381d45a6acbbb97497dd8ed63894
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2026-08-06 07:05:38 +0700

    fix: root layout bug and renew package-lock-.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import { type Metadata } from 'next';
-import { SessionProvider } from 'next-auth/react';
 import {
   Geist,
   Inter,

06cc0d9da1431c2550b422ca1ab5760796248776
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2026-08-06 14:58:13 +0700

    feat: added temporary docs for admin and non-admin users

--- src/app/docs/admin/layout.tsx
+++ src/app/docs/admin/layout.tsx
@@ -1,12 +1,41 @@
+import { Role } from '@prisma/client';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { ArrowLeft } from 'lucide-react';
+import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
 import { baseOptions } from '~/lib/layout.shared.admin';
 import { adminSource } from '~/lib/source';
+import { auth } from '~/server/auth';
 
-export default function Layout({ children }: LayoutProps<'/docs/admin'>) {
+export default async function Layout({ children }: LayoutProps<'/docs/admin'>) {
+  const session = await auth();
+  const isAdmin =
+    session?.user && (session.user.role === Role.ADMIN || session.user.role === Role.SUPERADMIN);
+
+  if (!session || !isAdmin) {
+    redirect('/dashboard');
+  } else if (!session.user.verified) {
+    redirect(`/auth/not-verified?email=${session.user.email}`);
+  }
+
   return (
-    <DocsLayout tree={adminSource.getPageTree()} {...baseOptions()}>
-      {children}
+    <DocsLayout
+      tree={adminSource.getPageTree()}
+      {...baseOptions()}
+      links={[
+        {
+          icon: <ArrowLeft className="size-4" />,
+          text: 'Back to Admin Dashboard',
+          url: '/admin',
+          active: 'none',
+        },
+      ]}
+      githubUrl="https://github.com/Himpunan-Mahasiswa-Mesin-ITB/hmm-lms"
+    >
+      <Suspense fallback={<div className="w-full h-full grid place-items-center">Loading...</div>}>
+        {children}
+      </Suspense>
     </DocsLayout>
   );
 }
--- src/app/docs/user/layout.tsx
+++ src/app/docs/user/layout.tsx
@@ -1,12 +1,42 @@
+import { Role } from '@prisma/client';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { ArrowLeft } from 'lucide-react';
+import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
 import { baseOptions } from '~/lib/layout.shared.user';
 import { userSource } from '~/lib/source';
+import { auth } from '~/server/auth';
 
-export default function Layout({ children }: LayoutProps<'/docs/user'>) {
+export default async function Layout({ children }: LayoutProps<'/docs/user'>) {
+  const session = await auth();
+
+  const isMachining = session?.user && session.user.role === Role.MACHINING;
+
+  if (isMachining) {
+    redirect('/machining');
+  } else if (!session) {
+    redirect('/auth/sign-in');
+  } else if (!session.user.verified) {
+    redirect(`/auth/not-verified?email=${session.user.email}`);
+  }
   return (
-    <DocsLayout tree={userSource.getPageTree()} {...baseOptions()}>
-      {children}
+    <DocsLayout
+      tree={userSource.getPageTree()}
+      {...baseOptions()}
+      links={[
+        {
+          icon: <ArrowLeft className="size-4" />,
+          text: 'Back to Dashboard',
+          url: '/dashboard',
+          active: 'none',
+        },
+      ]}
+      githubUrl="https://github.com/Himpunan-Mahasiswa-Mesin-ITB/hmm-lms"
+    >
+      <Suspense fallback={<div className="w-full h-full grid place-items-center">Loading...</div>}>
+        {children}
+      </Suspense>
     </DocsLayout>
   );
 }
